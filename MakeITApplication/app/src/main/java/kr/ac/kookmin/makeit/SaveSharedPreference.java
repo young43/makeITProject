298f914922305c974a1c83932448b5f23cb039db
young43 <seo5220@kookmin.ac.kr>
2020-11-02 20:25:18 +0900

    로그인 유지 관련 클래스 추가

---
+++ MakeITApplication/app/src/main/java/kr/ac/kookmin/makeit/SaveSharedPreference.java
@@ -0,0 +1,40 @@
+package kr.ac.kookmin.makeit;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * @file SaveSharedPreference
+ * @desc 자동 로그인 및 로그인 유지를 위한 클래스
+ * @auther 윤서영(20191633)
+ * @date 2020-11-02
+ */
+public class SaveSharedPreference {
+    static final String PREF_USER_NAME = "username";
+
+    static SharedPreferences getSharedPreferences(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx);
+    }
+
+    // 계정 정보 저장
+    public static void setUserName(Context ctx, String userName) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_NAME, userName);
+        editor.commit();
+    }
+
+    // 저장된 정보 가져오기
+    public static String getUserName(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_USER_NAME, "");
+    }
+
+    // 로그아웃
+    public static void clearUserName(Context ctx) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.clear();
+        editor.commit();
+    }
+
+}
