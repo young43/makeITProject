1c9dd15f066c05590b6abf16b379d24ce55ea5ca
young43 <seo5220@kookmin.ac.kr>
2020-11-12 18:03:33 +0900

    회원 중복체크

--- MakeITApplication/app/src/main/java/kr/ac/kookmin/makeit/SignUpActivity.java
+++ MakeITApplication/app/src/main/java/kr/ac/kookmin/makeit/SignUpActivity.java
@@ -13,9 +13,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 
@@ -25,6 +31,12 @@ public class SignUpActivity extends AppCompatActivity {
 
     Button btn_register;
     EditText emailText, idText, passwordText;
+
+    String id, pw, email, phone;
+
+    public interface MyDataCallback {
+        void onCallback(int result);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,38 +56,51 @@ public class SignUpActivity extends AppCompatActivity {
                 //회원가입 정보를 모두 입력했을 경우만 등록이 가능(정보를 모두 입력한 경우->EditText 길이가 5이상인 경우)
                 if(emailText.getText().toString().length()>4 && idText.getText().toString().length()>4 && passwordText.getText().toString().length()>4){
                     // Toast.makeText(SignUpActivity.this, "회원 가입이 완료되었습니다😀", Toast.LENGTH_SHORT).show();
-                    String id = idText.getText().toString().trim();
-                    String pw = passwordText.getText().toString();
-                    String email = emailText.getText().toString().trim();
-                    String phone = "010";
+                    id = idText.getText().toString().trim();
+                    pw = passwordText.getText().toString();
+                    email = emailText.getText().toString().trim();
+                    phone = "010";
 
-                    //(String id, String passwd, String email, String phone)
-                    UserInfo member = new UserInfo(id, pw, email, phone);
-                    HashMap<String, Object> data = (HashMap) member.toMap();
-
-                    // 파이어베이스 데이터(row) 추가
-                    db.collection("member")
-                        .add(data)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("member", "DocumentSnapshot written with ID: " + documentReference.getId());
-                                Toast.makeText(SignUpActivity.this, "회원 가입이 완료되었습니다😀", Toast.LENGTH_SHORT).show();
-                                finish();
+                    selectUserInfoOnFirebase(id, new MyDataCallback() {
+                        @Override
+                        public void onCallback(int result) {
+                            if(result < 0){
+                                Toast.makeText(SignUpActivity.this, "이미 회원정보가 존재합니다.", Toast.LENGTH_SHORT).show();
+                                return;
                             }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.w("member", "Error adding document", e);
-                            }
-                        });
 
 
-                    //로그인 페이지로 이동
-                    Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                    finish();
+                            //(String id, String passwd, String email, String phone)
+                            UserInfo member = new UserInfo(id, pw, email, phone);
+                            HashMap<String, Object> data = (HashMap) member.toMap();
+
+                            // 파이어베이스 데이터(row) 추가
+                            db.collection("member")
+                                    .add(data)
+                                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                        @Override
+                                        public void onSuccess(DocumentReference documentReference) {
+                                            Log.d("member", "DocumentSnapshot written with ID: " + documentReference.getId());
+                                            Toast.makeText(SignUpActivity.this, "회원 가입이 완료되었습니다😀", Toast.LENGTH_SHORT).show();
+                                            finish();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Log.w("member", "Error adding document", e);
+                                        }
+                                    });
+
+
+                            //로그인 페이지로 이동
+                            Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+
+
                 }
 
                 //회원 가입 정보를 모두 입력하지 않은 경우
@@ -87,6 +112,28 @@ public class SignUpActivity extends AppCompatActivity {
                 }
             }
         });
+
+    }
+
+
+
+    public void selectUserInfoOnFirebase(final String id, final MyDataCallback callback){
+        // Firebase 연동
+        // 회원정보 리스트를 긁어서 보여준다.
+        // Collection(=DB) -> Document(=row)으로 구성되어있으며, column은 getData로 Map형태로 가져올 수 있다.
+        CollectionReference collRef = db.collection("member");
+        Query query = collRef.whereEqualTo("id", id);
+        query.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                    int flag = 0;
+                    if(task.getResult().size() > 0)
+                        flag = -1;
+
+                    callback.onCallback(flag);
+                }
+            });
 
     }
 }
